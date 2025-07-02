ebc2f1cb884f632c02e8dd636518e73cf0a1aedc
aibelbin <aibelbinzacariah@gmail.com>
2025-07-02 10:41:37 +0530

    [FINAL] Day1 Question 1 Final push

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import pandas as pd
 csv = 'test.csv'
 
 file_content = pd.read_csv(csv)
+file_sorted = file_content.sort_values(by="Name")
 
+print(file_sorted)
+print("-------------------------------------------------")
 print(file_content)
