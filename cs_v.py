50704a39d527bf7f9d9d41dcf05a41833be13fe8
aibelbin <aibelbinzacariah@gmail.com>
2025-07-02 11:39:17 +0530

    [ADD] Day 1 Program 2 add

--- cs_v.py
+++ cs_v.py
@@ -1,10 +1,32 @@
-import pandas as pd 
+import csv
 
-csv = 'test.csv'
+csv_file = 'test.csv'
 
-file_content = pd.read_csv(csv)
-file_sorted = file_content.sort_values(by="Name")
+# Read the CSV file into a list
+with open(csv_file, newline='') as file:
+    reader = csv.reader(file)
+    data = list(reader)
 
-print(file_sorted)
+# Separate header and data rows
+header = data[0]
+rows = data[1:]
+
+# Find the index of the "Name" column
+name_index = header.index("Name")
+
+# Sort the data based on the "Name" column
+sorted_rows = sorted(rows, key=lambda row: row[name_index])
+
+# Print sorted data
+print("Sorted by Name:")
+print(header)
+for row in sorted_rows:
+    print(row)
+
 print("-------------------------------------------------")
-print(file_content)
+
+# Print original data
+print("Original Content:")
+print(header)
+for row in rows:
+    print(row)
